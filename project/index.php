a011a0475eaf9e345a8bb72d74a40d38ac883b36
Ahnaf Shahriar Abir <shahriarabir@Ahnafs-MacBook-Pro.local>
2018-09-18 13:48:21 +1000

    hi

---
+++ project/index.php
@@ -0,0 +1,28 @@
+<html> 
+<title>Weather</title>
+	<head><h1>Weather Today</h1></head>
+	<body> 
+		<form method="post" >
+		<div>
+			<p>Select Location:</p>
+			<select>
+			   <option value="null">choose one</option>
+			  <option value="vic">Victoria</option>
+			  <option value="melb">Melbourne</option>
+			  <option value="nsw">New South Wales</option>
+			  <option value="adl">Adelaide</option>
+			</select>
+		</div>
+		
+		<P></P>
+
+		<div><iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;height=300&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=c57108aogi53jce4nf8ihuho3g%40group.calendar.google.com&amp;color=%2323164E&amp;ctz=Australia%2FHobart" style="border-width:0" width="500" height="300" frameborder="0" scrolling="yes"></iframe></div>
+
+		<p></p>
+		<div>
+			<input type="submit" value="Search">
+		</div> 
+		</form>
+
+	</body> 
+</html>
